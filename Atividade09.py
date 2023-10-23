0a9109bf12a9e3cb6c79ab8f4b86f5f77920d8f9
Júlio César <140417209+juju-png@users.noreply.github.com>
2023-10-23 14:12:11 -0300

    Add files via upload

---
+++ Atividade09.py
@@ -0,0 +1,8 @@
+#Exercício Python 09: faça um software que verifique entre 2 numeros qual o maior
+x = int(input("digite um numero"))
+y = int (input("digite um numero"))
+
+if x > y : 
+    print(f" o numero {x} e maior que {y}") 
+else: 
+    print(f"o numero {y} e maior que {x}")
